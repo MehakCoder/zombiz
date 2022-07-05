cfc18c21a5688152e205d5f5139271e5c6d8db29
MehakCoder <cmehak26@gmail.com>
2022-07-05 12:43:39 +0530

    addd new file

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(document).ready(function () {
+        $('.counter').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 3500,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    });
+    $(window).on('load', function () {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 3000
+        });
+    });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    }) 
+});
