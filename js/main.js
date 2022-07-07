93df4ce0c93add761b7b67e0d3e9ba93b1791647
MehakCoder <cmehak26@gmail.com>
2022-07-07 18:43:09 +0530

    add style and image

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
- 1.   // counter
+$(document).ready(function () {
+    1.   // counter
     $(document).ready(function () {
         $('.counter').each(function () {
             $(this).prop('Counter', 0).animate({
@@ -20,61 +20,81 @@ $(document).ready(function(){
         });
     });
 
-   2.  // logo
+    2.  // logo
     $('#client').owlCarousel({
-        items:5,
-        loop:true,
+        items: 5,
+        loop: true,
         smartSpeed: 1000,
-        autoplay:true,
-        dots:false,
-        autoplayHoverPause:true,
-        responsive:{
-                0:{
-                    items:2
-                },
-                415:{
-                    items:2
-                },
-                600:{
-                    items:3
-                },
-                1000:{
-                    items:5
-                }
+        autoplay: true,
+        dots: false,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            415: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
             }
-        });
+        }
     });
-    
-  3. //scroll to top  
-  $(window).on('scroll',function () {
+});
+
+3. //scroll to top  
+$(window).on('scroll', function () {
     if ($(this).scrollTop() > 600) {
         $('.return-to-top').fadeIn();
     } else {
         $('.return-to-top').fadeOut();
     }
 });
-$('.return-to-top').on('click',function(){
-        $('html, body').animate({
+$('.return-to-top').on('click', function () {
+    $('html, body').animate({
         scrollTop: 0
     }, 100);
     return false;
 });
 4. //testimonial
 $("#testimonial-sliders").owlCarousel({
+    loop: true,
+    margin: 100,
+    nav: false,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+$("#team-carousel").owlCarousel({
     loop:true,
-    margin:100,
+    margin:10,
     nav:false,
-   dots:true,
+    dots:false,
+    touchDrag:true,
+    autoplay:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:1
+            items:4
         }
-    }    
+    }
 });
 
+5.//team
